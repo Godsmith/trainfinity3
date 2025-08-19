e1df589300cfa58e17880cffae935e6efa5cbb61
Filip Lange <filip.lange@gmail.com>
2025-08-19 22:42:56 +0200

    Add rudimentary pathfinding between stations

--- scenes/game.gd
+++ scenes/game.gd
@@ -5,19 +5,22 @@ const SCALE_FACTOR := 2  # Don't remember where I set this
 var is_left_mouse_button_held_down := false
 var is_right_mouse_button_held_down := false
 var start_track_location := Vector2()
-enum GUI_STATE {NONE, TRACK}
+enum GUI_STATE {NONE, TRACK, STATION}
 var gui_state := GUI_STATE.NONE
 var ghost_tracks: Array[Track] = []
 var tracks: Array[Track] = []
 var ghost_track_tile_positions: Array[Vector2] = []
-
+var astar = AStar2D.new()
 var track_scene = preload("res://scenes/track.tscn")
+const STATION = preload("res://scenes/station.tscn")
+var astar_id_from_position = {}
+
 
 func snap_to_grid(position: Vector2) -> Vector2:
 	return Vector2(round(position.x/TILE_SIZE)*TILE_SIZE, round(position.y/TILE_SIZE)*TILE_SIZE)
 
 
-func _input(event: InputEvent) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			is_left_mouse_button_held_down = event.is_pressed()
@@ -27,10 +30,11 @@ func _input(event: InputEvent) -> void:
 			is_right_mouse_button_held_down = event.is_pressed()
 	
 	if event is InputEventMouseButton and event.is_released() and event.button_index == MOUSE_BUTTON_LEFT:
-		for ghost_track in ghost_tracks:
-			tracks.append(ghost_track)
-			ghost_track.modulate = Color(1,1,1,1)
-		ghost_tracks.clear()
+		match gui_state:
+			GUI_STATE.TRACK:
+				_create_track()
+			GUI_STATE.STATION:
+				_create_station(snap_to_grid(get_local_mouse_position()))
 	
 	if event is InputEventMouseMotion:
 		$GhostTrack.position = snap_to_grid(get_local_mouse_position())
@@ -43,6 +47,45 @@ func _input(event: InputEvent) -> void:
 		if is_right_mouse_button_held_down:
 			var camera = get_viewport().get_camera_2d()
 			camera.position -= event.get_relative() / SCALE_FACTOR
+			
+func _create_track():
+	for ghost_track in ghost_tracks:
+		tracks.append(ghost_track)
+		ghost_track.modulate = Color(1,1,1,1)
+	var ids = []
+	for i in len(ghost_track_tile_positions):
+		var position = ghost_track_tile_positions[i]
+		_add_position_to_astar(position)
+		ids.append(astar_id_from_position[position])
+	for i in range(1, len(ids)):
+		astar.connect_points(ids[i-1], ids[i])
+		print_debug("connected " + str(ids[i]) + " with " + str(ids[i-1]))
+	ghost_tracks.clear()
+	
+func _add_position_to_astar(position):
+	if not position in astar_id_from_position:
+		var id = astar.get_available_point_id()
+		astar_id_from_position[position] = id
+		astar.add_point(id, position)
+		#print_debug("position " + str(position) + " added as id " + str(id))
+	
+	
+func _create_station(position: Vector2):
+	var station = STATION.instantiate()
+	station.position = position
+	_add_position_to_astar(position)
+	station.station_clicked.connect(_station_clicked)
+	add_child(station)
+	
+func _station_clicked(station: Station):
+	var id1 = astar_id_from_position[station.position.round()]
+	for other_station: Station in get_tree().get_nodes_in_group("stations"):
+		station.modulate = Color(1,1,1,1)
+	for other_station: Station in get_tree().get_nodes_in_group("stations"):
+		if other_station != station:
+			var id2 = astar_id_from_position[other_station.position.round()]
+			if astar.get_point_path(id1, id2):
+				other_station.modulate = Color(0,1,0,1)
 
 func _show_ghost_track(positions: Array[Vector2]):
 	
@@ -79,11 +122,12 @@ func _positions_between(start: Vector2, stop: Vector2) -> Array[Vector2]:
 		
 	
 
-func _on_railbutton_toggled(toggled_on: bool) -> void:
-	if toggled_on: 
-		gui_state = GUI_STATE.TRACK
-	else:
-		gui_state = GUI_STATE.NONE
-	$GhostTrack.visible = toggled_on
+func _on_trackbutton_toggled(toggled_on: bool) -> void:
+	_change_gui_state(GUI_STATE.TRACK if toggled_on else GUI_STATE.NONE)
+
+func _on_stationbutton_toggled(toggled_on: bool) -> void:
+	_change_gui_state(GUI_STATE.STATION if toggled_on else GUI_STATE.NONE)
 		
-	
+func _change_gui_state(new_state: GUI_STATE):
+	gui_state = new_state
+	$GhostTrack.visible = (new_state == GUI_STATE.TRACK)
