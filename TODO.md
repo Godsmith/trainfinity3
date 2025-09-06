6c73e407318b498cd396efe14e9ff884b7dcce45
Filip Lange <filip.lange@gmail.com>
2025-09-06 14:32:03 +0200

    Move TODO to separate file

---
+++ TODO.md
@@ -0,0 +1,21 @@
+# In-game upgrades
+- range of stations (start with 1) - though it looks ugly when they are too far from ore
+- number of trains (start with 1)
+- map size
+- the ability to tear down/build track on mountain
+- bridges
+
+# Between-round upgrade
+- starting everything above
+- amount of ore per tile
+
+# TODO
+- larger stations
+- cities
+- iron
+- prevent scrolling past edges of the map
+  - note that duplicating the map size and zooming out max makes the game stutter, so adjustments needed. 
+    At least in debug mode, need to test performance after exporting as well.
+- saving the game
+- train collisions
+  - cannot be done until we have pathfinding that takes other trains into account/signals/manual paths
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,25 +1,3 @@
-# In-game upgrades
-# - range of stations (start with 1) - though it looks ugly when they are too far from ore
-# - number of trains (start with 1)
-# - map size
-# - the ability to tear down/build track on mountain
-# - bridges
-
-# Between-round upgrade
-# - starting everything above
-# - amount of ore per tile
-
-# TODO
-# - prevent scrolling past edges of the map
-#   - note that duplicating the map size and zooming out max makes the game stutter, so adjustments needed. 
-#	  At least in debug mode, need to test performance after exporting as well.
-# - saving the game
-# - cities
-# - iron
-# - train collisions
-#   - cannot be done until we have pathfinding that takes other trains into account/signals/manual paths
-
-
 extends Node2D
 
 class_name Game
